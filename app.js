fb999cd3b61c19b3ad8e2775702d10e71dc47773
gon-khaung <gonkhaung19@gmail.com>
2022-03-08 00:28:32 +0630

    added ejs and test

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const express = require("express");
 //express app
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
+//listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./view/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./view/about.html", { root: __dirname });
+  res.render("about");
 });
 
 app.get("/about-us", (req, res) => {
@@ -18,5 +22,5 @@ app.get("/about-us", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./view/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
